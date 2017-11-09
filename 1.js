d660f22ca641186bb46c7ebc10ec41f866b989ca
Natalia Balabonkina <nataliabalabonkina@gmail.com>
2017-11-09 12:34:52 +0200

    first commit

---
+++ 1.js
@@ -0,0 +1,14 @@
+function one(){
+  var a1;
+  var a2;
+a1=document.getElementById('inp1').value;
+a2=document.getElementById('inp2').value;
+    parseInt(a1);
+    parseInt(a2);
+if (a1=="ivan" && a2==333){ alert('Добро пожаловать');}
+else if (a1=="ssss" && a2==666){ alert('Добро пожаловать');}
+else if (a1=="gibs" && a2==0000){ alert('Добро пожаловать');}
+else{
+    alert('ошибка');
+}
+}
